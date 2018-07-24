dd9a24d875bdee97fb6af9e08685a92e4eb3c26f
Marcel Partosoebroto <m.partosoebroto@coolblue.nl>
2018-07-24 21:33:00 +0200

    First sketch mobile cards

--- public/index.php
+++ public/index.php
@@ -7,11 +7,51 @@
     </head>
     <body>
         <header>
-            <h1>Marcel's website</h1>
-            <h5>Sneakers 'n shit</h5>
+            <h3 class="title is-3">Sneakers 'n shit</h3>
         </header>
-        <footer>
-
-        </footer>
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-256x256">
+                    <img src="https://pbs.twimg.com/media/DFmNkjvU0AAYixV.jpg" alt="Sample image1">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">Nike Flyknit Trainer</p>
+                        <p class="subtitle is-6">€150,-</p>
+                        <p class="subtitle is-6">Release date: 24/07/2018</p>
+                    </div>
+                </div>
+                <div class="content">
+                    <p>Available at:</p>
+                    <a>website</a><br>
+                    <a>website</a><br>
+                    <a>website</a><br>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-256x256">
+                    <img src="https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto/zwnqu2emjltkspwxjcvs/air-max-93-herenschoen-CJ5htx.jpg" alt="Sample image2">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">Nike Air Max 93</p>
+                        <p class="subtitle is-6">€145,-</p>
+                        <p class="subtitle is-6">27/07/2018</p>
+                    </div>
+                </div>
+                <div class="content">
+                    <p>Available at:</p>
+                    <a>website</a><br>
+                    <a>website</a><br>
+                    <a>website</a><br>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
