672e8c2aecbc71c53d2a47e60f2d9ae94f8b9b1b
Marcel Partosoebroto <m.partosoebroto@coolblue.nl>
2018-07-24 16:25:04 +0200

    First commit, yay

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Marcel's Website</title>
+        <link rel="stylesheet" href="/assets/main.css">
+        <meta name="viewport" content="width=device-width,user-scalable=0">
+    </head>
+    <body>
+        <header>
+            <h1>Marcel's website</h1>
+            <h5>Sneakers 'n shit</h5>
+        </header>
+        <footer>
+
+        </footer>
+    </body>
+</html>
