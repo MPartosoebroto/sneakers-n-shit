3ec5a91beee2adf2ae265e4df1bec2c9f13bb3b7
Marcel Partosoebroto <m.partosoebroto@coolblue.nl>
2018-08-07 16:41:45 +0200

    Add new elements to index

--- public/index.php
+++ public/index.php
@@ -1,14 +1,72 @@
 <!doctype html>
 <html>
 <head>
-    <title>Pattasoebroto - Sneaker releasesgit</title>
+    <title>Pattasoebroto - Sneaker release dates</title>
     <link rel="stylesheet" href="/assets/main.css">
     <meta name="viewport" content="width=device-width,user-scalable=0">
 </head>
 <body>
 <header>
-    <div class="centered"><h5 class="title is-5">Pattasoebroto</h5></div>
+    <section class="hero is-primary is-medium">
+        <div class="hero-head">
+            <nav class="navbar">
+                <div class="container">
+                    <div class="navbar-brand">
+                        <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+                    </div>
+                    <div id="navbarMenuHeroA" class="navbar-menu">
+                        <div class="navbar-end">
+                            <a class="navbar-item is-active">
+                                Home
+                            </a>
+                            <a class="navbar-item">
+                                Examples
+                            </a>
+                            <a class="navbar-item">
+                                Documentation
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title">
+                    Pattasoebroto
+                </h1>
+                <h2 class="subtitle">
+                    Sneaker release dates
+                </h2>
+            </div>
+        </div>
+        <div class="hero-foot">
+            <nav class="tabs">
+                <div class="container">
+                    <ul>
+                        <li class="is-active"><a>Overview</a></li>
+                        <li><a>News</a></li>
+                        <li><a>Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </section>
 </header>
+<div class="field">
+    <div class="control">
+        <div class="select is-small">
+            <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+            </select>
+        </div>
+    </div>
+</div>
 <div class="products">
     <div class="centered">
         <div class="tile">
@@ -94,6 +152,112 @@
                         </div>
                         <div class="content">
                             <p class="is-marginless available">Available at:</p>
+                            <a class ="website">website</a> | <a class ="website">website</a> | <a class ="website">website</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card tile is-vertical is-child is-4">
+                    <div class="card-image">
+                        <figure class="image">
+                            <img src="https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto/zwnqu2emjltkspwxjcvs/air-max-93-herenschoen-CJ5htx.jpg"
+                                 alt="Sample image2">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4 is-marginless">Nike Air Max 93</p>
+                                <p class="subtitle is-6 is-marginless"><span class="product-price">€145,-</span></p>
+                                <p class="subtitle is-6"><span class="release-date">Release date: <b>27/07/2018</b></span></p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <p class="is-marginless available">Available at:</p>
+                            <a class="website">website</a> | <a class="website">website</a> | <a class="website">website</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card tile is-vertical is-child is-4">
+                    <div class="card-image">
+                        <figure class="image is-256x256">
+                            <img src="https://pbs.twimg.com/media/DFmNkjvU0AAYixV.jpg" alt="Sample image1">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4 is-marginless">Nike Flyknit Trainer</p>
+                                <p class="subtitle is-6 is-marginless"><span class="product-price">€150,-</span></p>
+                                <p class="subtitle is-6"><span class="release-date">Release date: <b>24/07/2018</b></span></p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <p class="is-marginless available">Available at:</p>
+                            <a class="website">website</a> | <a class="website">website</a> | <a class="website">website</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tile">
+            <div class="tile is-parent is-paddingless">
+                <div class="card tile is-vertical is-child is-4">
+                    <div class="card-image">
+                        <figure class="image is-256x256">
+                            <img src="https://pbs.twimg.com/media/DFmNkjvU0AAYixV.jpg" alt="Sample image1">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4 is-marginless">Nike Flyknit Trainer</p>
+                                <p class="subtitle is-6 is-marginless"><span class="product-price">€150,-</span></p>
+                                <p class="subtitle is-6"><span class="release-date">Release date: <b>24/07/2018</b></span></p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <p class="is-marginless available">Available at:</p>
+                            <a class ="website">website</a> | <a class ="website">website</a> | <a class ="website">website</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card tile is-vertical is-child is-4">
+                    <div class="card-image">
+                        <figure class="image">
+                            <img src="https://c.static-nike.com/a/images/t_PDP_864_v1/f_auto/zwnqu2emjltkspwxjcvs/air-max-93-herenschoen-CJ5htx.jpg"
+                                 alt="Sample image2">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4 is-marginless">Nike Air Max 93</p>
+                                <p class="subtitle is-6 is-marginless"><span class="product-price">€145,-</span></p>
+                                <p class="subtitle is-6"><span class="release-date">Release date: <b>27/07/2018</b></span></p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <p class="is-marginless available">Available at:</p>
+                            <a class="website">website</a> | <a class="website">website</a> | <a class="website">website</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card tile is-vertical is-child is-4">
+                    <div class="card-image">
+                        <figure class="image is-256x256">
+                            <img src="https://pbs.twimg.com/media/DFmNkjvU0AAYixV.jpg" alt="Sample image1">
+                        </figure>
+                    </div>
+                    <div class="card-content">
+                        <div class="media">
+                            <div class="media-content">
+                                <p class="title is-4 is-marginless">Nike Flyknit Trainer</p>
+                                <p class="subtitle is-6 is-marginless"><span class="product-price">€150,-</span></p>
+                                <p class="subtitle is-6"><span class="release-date">Release date: <b>24/07/2018</b></span></p>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <p class="is-marginless available">Available at:</p>
                             <a class="website">website</a> | <a class="website">website</a> | <a class="website">website</a>
                         </div>
                     </div>
@@ -102,8 +266,27 @@
         </div>
     </div>
 </div>
-<footer>
-    <div class="centered"><h5 class="title is-5">Pattasoebroto</h5></div>
+<nav class="pagination is-small" role="navigation" aria-label="pagination">
+    <a class="pagination-previous">Previous</a>
+    <a class="pagination-next">Next page</a>
+    <ul class="pagination-list">
+        <li>
+            <a class="pagination-link is-current" aria-label="Page 1" aria-current="page">1</a>
+        </li>
+        <li>
+            <a class="pagination-link" aria-label="Goto page 2">2</a>
+        </li>
+        <li>
+            <a class="pagination-link" aria-label="Goto page 3">3</a>
+        </li>
+    </ul>
+</nav>
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p>
+            <strong>Pattasoebroto</strong> by Marcel Partosoebroto.
+        </p>
+    </div>
 </footer>
 </body>
 </html>
